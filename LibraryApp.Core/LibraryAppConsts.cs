a826e72998d47f5d0c608b738ce55293c60d4e2b
Will Haney <whaney@trainingconsultants.net>
2018-03-19 07:47:56 -0700

    Add project files.

---
+++ LibraryApp.Core/LibraryAppConsts.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryApp
+{
+    public class LibraryAppConsts
+    {
+        public const string LocalizationSourceName = "LibraryApp";
+
+        public const bool MultiTenancyEnabled = true;
+    }
+}
